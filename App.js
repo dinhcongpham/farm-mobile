d0bfd6661d4ea528337277072bc50d15b2bc0867
cong <phamdinhcong0@gmail.com>
2023-12-25 22:15:23 +0700

    fix dot hidden

--- App.js
+++ App.js
@@ -1,21 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!!!!!!!0!</Text>
-      <Text>Vai ca dai a</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+import MyBezierLineChart  from './src/MyBezierLineChart.js';
+
+
+const pH = {
+  label: 'pH',
+  upperLimit: 9,
+  lowerLimit: 7,
+  sql: 'SELECT * FROM sensorParserSmartWater WHERE sensor = "PH" ORDER BY id DESC LIMIT 30'
 }
+const DO = {
+  label: 'DO (mg/l)',
+  upperLimit: null,
+  lowerLimit: 3.5,
+  sql: 'SELECT * FROM sensorParserSmartWater WHERE sensor = "DO" ORDER BY id DESC LIMIT 30'
+}
+const Temperature = {
+  label: 'Temperature (°C)',
+  upperLimit: 33,
+  lowerLimit: 18,
+  sql: 'SELECT * FROM sensorParserSmartWater WHERE sensor = "WT" ORDER BY id DESC LIMIT 30'
+}
+const Turbidity = {
+  label: 'Turbidity (V)',
+  upperLimit: 4.2,
+  lowerLimit: 2.5,
+  sql: 'SELECT * FROM sensorParserSmartWater WHERE sensor = "TUR" ORDER BY id DESC LIMIT 30'
+}
+const Alkalinity = {
+  label: 'Alkalinity (mg/l)',
+  upperLimit: 180,
+  lowerLimit: 60,
+  sql: 'SELECT * FROM sensorParserSmartWater WHERE sensor = "COND" ORDER BY id DESC LIMIT 30'
+}
+const Salinity = {
+  label: 'Salinity (‰)',
+  upperLimit: 35,
+  lowerLimit: 5,
+  sql: 'SELECT * FROM sensorParserSmartWater WHERE sensor = "COND" ORDER BY id DESC LIMIT 30'
+}
+
+function App() {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <ScrollView>
+        <View style={styles.container}>
+          <View>
+            <MyBezierLineChart props={pH}/>
+            <MyBezierLineChart props={DO}/>
+            <MyBezierLineChart props={Temperature}/>
+            <MyBezierLineChart props={Turbidity}/>
+            <MyBezierLineChart props={Alkalinity}/>
+            <MyBezierLineChart props={Salinity}/>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'white',
     justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: 20
   },
-});
+  header: {
+    textAlign: 'center',
+    fontSize: 18,
+    padding: 10
+  }
+})
---
+++ src/MyBezierLineChart.js
@@ -0,0 +1,255 @@
+import React from 'react'
+import {
+  Text,
+  StyleSheet,
+  Dimensions,
+  View,
+} from 'react-native'
+import {
+  LineChart
+} from 'react-native-chart-kit'
+
+import {
+  Rect, 
+  Text as TextSVG, 
+  Svg,
+  Line,
+  Circle
+} from 'react-native-svg'
+
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const apiUrl = 'https://9bd5-115-73-218-247.ngrok-free.app/query'
+const isNumber = value => !isNaN(parseFloat(value)) && isFinite(value)
+
+const formatTimestamp = (timestamp) => {
+  const date = new Date(timestamp);
+
+  const day = date.getUTCDate();
+  const month = date.getUTCMonth() + 1; // Month is zero-based, so we add 1
+  const hour = date.getUTCHours();
+  const minute = date.getUTCMinutes();
+  const second = date.getUTCSeconds();
+
+  // Format the components with leading zeros if needed
+  const formattedDate = `${day < 10 ? '0' : ''}${day}/${month < 10 ? '0' : ''}${month}  ${hour}:${minute}:${second}`;
+
+  return formattedDate;
+}
+
+const  MyBezierLineChart = (props) => {
+
+  const { label, sql } = props.props;
+
+  const [xData, setXData] = useState([1])
+  const [yData, setYData] = useState([1])
+  const [flag, setFlag] = useState(true)
+  const [selectedDataIndex, setSelectedDataIndex] = useState(null)
+
+  let [tooltipPos, setTooltipPos] = useState({
+    x: 0,
+    y: 0,
+    visible: false,
+    label,
+    value: 0,
+  });
+
+  const fullUrl = `${apiUrl}?sql=${encodeURIComponent(sql)}`
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(fullUrl, {
+        headers: {
+          'Content-Type': 'application/json',
+          'ngrok-skip-browser-warning': 'true',
+          Accept: 'application/json'
+        }
+      })
+
+      if (!response.data) {
+        throw new Error('Empty response data')
+      }
+
+      const dataArray = response.data
+
+      if (dataArray && Array.isArray(dataArray)) {
+        const newData = dataArray.map(data => {
+          const { value, timestamp } = data
+          const timestampDate = formatTimestamp(timestamp)
+          if (isNumber(value)) {
+            return {
+              x: timestampDate,
+              y: label === 'Alkalinity (mg/l)' ?
+                ( (parseFloat(value) * 0.64 ) / 0.6).toFixed(2) : label === 'Salinity (‰)' ?
+                  (parseFloat(value) * 0.00064).toFixed(2) : label === 'DO (mg/l)' ?
+                    (parseFloat(value) * 1.33).toFixed(2) : parseFloat(value)
+            }
+          }
+          else {
+            return {
+              x: timestampDate,
+              y: parseFloat(4)
+            }
+          }
+        })
+        newData.sort((a, b) => a.x - b.x)
+        setXData(newData.map(item => item.x))
+        setYData(newData.map(item => item.y))
+      }
+
+    } catch (error) {
+      console.error('Axios error:', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+
+    // set time to call api and get data
+    const intervalId = setInterval(() => {
+      setFlag(!flag)
+    }, 500000)
+
+    return () => {
+      clearInterval(intervalId)
+    }
+
+  }, [flag])
+
+
+  return (
+    <>
+      <Text style={styles.header}>{label}</Text>
+      <LineChart
+        data={{
+          labels: xData,
+          datasets: [
+            {
+              data: yData,
+            },
+            {
+              data: [Math.min(...yData) - 0.1],
+              color: () => 'transparent',
+              strokeWidth: 0,
+              withDots: false,
+            },
+            {
+              data: [Math.max(...yData) + 0.1],
+              color: () => 'transparent',
+              strokeWidth: 0,
+              withDots: false,
+            },
+          ],
+        }}
+        width={Dimensions.get('window').width - 16}
+        height={220}
+        chartConfig={{
+          backgroundColor: '#1cc910',
+          backgroundGradientFrom: '#eff3ff',
+          backgroundGradientTo: '#efefef',
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 255) => `rgba(0, 0, 0, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+          propsForDots: {
+            r: "3",
+            strokeWidth: "2",
+          }, 
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+        hidePointsAtIndex={xData.map((item, idx) => (idx !== 1) && idx)}
+        decorator={() => {
+
+          const rectWidth = 130
+          const rectHeight = 40
+          const rectX = tooltipPos.x - 15
+          const rectY = tooltipPos.y + 10
+
+          return tooltipPos.visible ? (
+            <View>
+              <Svg>
+                <Rect
+                  x={rectX}
+                  y={rectY}
+                  width={rectWidth}
+                  height={rectHeight}
+                  fill="white"
+                />
+                <Line
+                  x1={rectX}
+                  y1={rectY + rectHeight / 2}
+                  x2={rectX + rectWidth}
+                  y2={rectY + rectHeight / 2}
+                  stroke="black"
+                  strokeWidth="0.3"
+                />
+                <TextSVG
+                  x={rectX + rectWidth / 2}
+                  y={rectY + rectHeight / 2 + 14}
+                  fill="black"
+                  fontSize="13"
+                  textAnchor="middle">
+                  {`${label} : ${tooltipPos.value}`}
+                </TextSVG>
+                <TextSVG
+                  x={rectX + rectWidth / 2}
+                  y={rectY + rectHeight / 2 - 5}
+                  fill="black"
+                  fontSize="13"
+                  textAnchor="middle">
+                  {`${tooltipPos.label}`}
+                </TextSVG>
+              </Svg>
+            </View>
+          ) : null;
+        }}
+        onDataPointClick={data => {
+          let isSamePoint = tooltipPos.x === data.x && tooltipPos.y === data.y;
+          
+          isSamePoint
+            ? setTooltipPos(previousState => {
+                return {
+                  ...previousState,
+                  value: data.value,
+                  label: xData[data.index],
+                  visible: !previousState.visible,
+                };
+              })
+            : setTooltipPos({
+                x: data.x,
+                value: data.value,
+                label: xData[data.index],
+                y: data.y,
+                visible: true,
+              });
+              
+        }}
+      />
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: 20
+  },
+  header: {
+    textAlign: 'center',
+    fontSize: 18,
+    padding: 10
+  }
+})
+
+
+export default MyBezierLineChart;
